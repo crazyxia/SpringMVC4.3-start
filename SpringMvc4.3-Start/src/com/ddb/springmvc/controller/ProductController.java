5b2c8db2f0c103b4cb6c32c891aec2a7934f803c
xiachaochao <2252916592@qq.com>
2017-06-30 19:32:02 +0800

    springmvc资料上传

--- SpringMvc4.3-Start/src/com/ddb/springmvc/controller/ProductController.java
+++ SpringMvc4.3-Start/src/com/ddb/springmvc/controller/ProductController.java
@@ -4,7 +4,10 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.ddb.springmvc.form.ProductForm;
 import com.ddb.springmvc.service.ProductService;
 
 @Controller
@@ -17,5 +20,9 @@ public class ProductController {
 	public String inputProduct(){
 		logger.info("inputProduct被调用！");
 		return "ProductForm";
+	}
+	@RequestMapping(value="/product_save",method=RequestMethod.POST)
+	public String saveProduct(ProductForm productForm,RedirectAttributes redirectAttributes){
+		logger.info("saveProduct被调用");
 	}
 }
